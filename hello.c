a4cc8412c9877ce9624d1c88aea62d428e092219
EthanE <eenglish8@huskers.unl.edu>
2021-08-21 21:34:30 +0000

    changed message

--- hello.c
+++ hello.c
@@ -1,17 +1,17 @@
 /**
  * Author: Ethan
- * 
+ *
  * Date: 2021
  *
  * A simple hello world program in C
- * 
+ *
  */
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argx, char **argv) {
-    
+
     printf("Ethan English\n");
-    
+
     return 0;
 }
