56b2acc5412c335d538aee7ffd098ed68b0c3054
EthanE <eenglish8@huskers.unl.edu>
2021-08-21 18:00:17 +0000

    inital commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Ethan
+ * 
+ * Date: 2021
+ *
+ * A simple hello world program in C
+ * 
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argx, char **argv) {
+    
+    printf("Hello World!\n");
+    
+    return 0;
+}
