462c5912e691ed1ea3fe90c350afd7eae1bf4cb8
EthanE <eenglish8@huskers.unl.edu>
2021-08-29 15:21:14 +0000

    added my major to output

--- hello.c
+++ hello.c
@@ -1,17 +1,16 @@
 /**
- * Author: Ethan
- *
- * Date: 2021
- *
- * A simple hello world program in C
- *
+ *Author: Ethan English 
+ *Date: 2021/8/29
+ * 
  */
+ 
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argx, char **argv) {
 
-    printf("Ethan English\n");
+    printf("My name is Ethan English\n");
+    printf("My major is computer engineering")
 
     return 0;
 }
